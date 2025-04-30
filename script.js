658f5dd937b564ba053ba42621cad0d7a919ac5b
chromydavid <chromy.david1@seznam.cz>
2025-04-30 09:25:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ function create_size_inputs(){
     const count = parseInt(document.getElementById("number_of_Lans").value);
     const container = document.getElementById("lan_size_container");
     for (let i = 0; i<count;i++){
+        const text = document.createElement("p"));
+        text.textContent = "Lan " + i;
+        container.appendChild(text);
         const input = document.createElement("input");
         input.type = "number";
         input.id = "lan_size_"+i ;
