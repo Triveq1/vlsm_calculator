5d45b83a44f56d62655a80f906c9daf476b13e2f
chromydavid <chromy.david1@seznam.cz>
2025-04-30 16:40:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var login_table = {
-    "admin":"1234"
+  "admin":"admin"
 };
 
-function bubbleSort(arr) {
+function bubble_sort(arr) {
   let n = arr.length;
   let swapped;
   do {
@@ -20,75 +20,29 @@ function bubbleSort(arr) {
 }
 
 
+//This function generates 3 columns of n rows of input boxes for host count of each LAN
+function generate_network_size_input_boxes(){
+  const number_of_networks = parseInt(document.getElementById("number_of_networks").value);
+  const container = document.getElementById("network_size_input_boxes_container");
+  container.innerHTML = "";
 
-function create_size_inputs(){
-    const count = parseInt(document.getElementById("number_of_Lans").value);
-    const container = document.getElementById("lan_size_container");
-    container.innerHTML = "";
-    for (let i = 0; i<count;i++){
-        const input = document.createElement("input");
-        input.type = "number";
-        input.id = "lan_size_"+i ;
-        input.placeholder = "LAN "+i+" size";
-        container.appendChild(input);
-    }
+  for (let i = 1; i <= number_of_networks ; i++ ){
+      const row = document.createElement("div");
+      row.className = "input_network_size_row";
+
+      const box = document.createElement("input");
+      box.type = "number";
+      box.id = "network_"+i+"_size" ; // network_5_size 
+      box.placeholder = "hosts";
+      box.className = "input_network_size_box"
+
+      const box_name = document.createElement("p");
+      box_name.textContent = "LAN "+i+": ";
+
+      row.appendChild(box_name);
+      row.appendChild(box);
+      container.appendChild(row);
+  }
 }
 
-function calculate_vlsm() {
-    const arr_of_base2 = [2, 4, 8, 16, 32, 64, 128, 256];
-    const container =  documnet.getElementById("vlsm_table");
-    container.innerHTML = "";
-    const lans = parseInt(document.getElementById("number_of_Lans").value);
-    const table = document.createElement("table");
-    table.border = "1";
-    var lan_array = [];
 
-    for (let f = 0; f<lans; f++){
-        lan_array.push(document.getElementById("lan_size_"+f).value);
-    }
-    console.log(len_array);
-    lan_array = bubbleSort(len_array);
-    console.log(len_array);
-
-    var current_net = 0;
-    var lan_net_arr = [];
-    for (let a = 0 ; i<lans; a++){
-        var size = lan_array[a];
-        for (num = 0 ; num < 8; num++);
-            if (size <= arr_of_base2[num]) {
-                lan_net_arr.push(current_net);
-                current_net += arr_of_base2[num];
-            }
-    }
-
-
-    
-    for (let i = 0 ; i<lans ; i++){
-        const row = document.createElement("tr");
-
-        const e_lan_number = document.createElement("td");
-        const e_host_count = document.createElement("td");
-        const e_network_adr = document.createElement("td");
-        const e_usable_range = document.createElement("td");
-        const e_broadcast = document.createElement("td");
-        const e_mask = document.createElement("td");
-            
-        e_lan_number.textContent = "Lan "+i;
-            
-        var a = "lan_size_"+i
-        var host_count = parseInt(document.getElemntById(a).value);
-        e_host_count.textContent = ""+host_count;
-        host_count += 2;
-
-        e_network_adr.textContent = document.getElementById("ipv41").value + document.getElementById("ipv42").value + document.getElementById("ipv43").value + lan_net_arr[i];
-        row.appendChild(e_lan_number);
-        row.appendChild(e_host_count);
-        row.appendChild(e_network_adr);
-        row.appendChild(e_usable_range);
-        row.appendChild(e_broadcast);
-        row.appendChild(e_mask);
-        
-        table.appendChild(row);
-    }
-    container.appendChild(table);
-}
