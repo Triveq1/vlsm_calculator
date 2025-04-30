757416dc6d1ae0522d9c09f829df956621d98a0f
chromydavid <chromy.david1@seznam.cz>
2025-04-30 08:40:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const users = new Map([
-    ["Triveq", "7894561235"],
-    ["Leo", "123456789"],
-  ]);
-  
-  const loginForm = document.getElementById("loginForm");
-  const errorText = document.getElementById("error");
-  const secretMessage = document.getElementById("secretMessage");
-  
-  loginForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-  
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value;
-  
-    if (users.has(username) && users.get(username) === password) {
-      errorText.textContent = "";
-      loginForm.style.display = "none";
-      secretMessage.classList.remove("hidden");
-    } else {
-      errorText.textContent = "Invalid username or password.";
+var login_table = {
+    "admin":"1234"
+};
+
+function create_size_inputs(){
+    const count = parseInt(document.getElementById("number_of_Lans").value);
+    const container = document.getElementById("lan_size_container");
+    for (let i = 0; i<count;i++){
+        const input = document.createElement("input");
+        input.type = "number"
     }
-  });
+}
+
+
+<button onclick="create_size_inputs()">Generate table</button>
+      <div id="lan_size_container"></div>
